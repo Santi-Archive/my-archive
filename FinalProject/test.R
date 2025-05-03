115a6a9ab18c90b9fc4ac6401c8b0a8a816b700b
wobinwob <152148300+wobinwob@users.noreply.github.com>
2025-05-03 14:46:50 +0800

    added to-do list and updated code

--- FinalProject/test.R
+++ FinalProject/test.R
@@ -8,53 +8,69 @@ data4 <- read_csv("FinalProject/France/2024_VNL_TUR_vs_FRA.csv")
 
 combined_data <- bind_rows(data1, data2, data3, data4)
 
-france_score_counts <- combined_data %>%
+test_france_score_counts <- combined_data %>%
   filter(scoring_team == "France") %>%
   count(score_type, sort = TRUE)
 
-opponent_score_counts <- combined_data %>%
+test_opponent_score_counts <- combined_data %>%
   filter(scoring_team != "France") %>%
   count(score_type, sort = TRUE)
 
-france_serve_ace_count <- france_score_counts %>%
+test_france_serve_ace_count <- test_france_score_counts %>%
   filter(score_type == "serve_ace" & !is.na(n)) %>%
   pull(n)
 
-france_zone2_attack_count <- france_score_counts %>%
-  filter(score_type == "attack_zone2" & !is.na(n)) %>%
-  pull(n)
-
-france_zone3_attack_count <- france_score_counts %>%
-  filter(score_type == "attack_zone3" & !is.na(n)) %>%
-  pull(n)
-
-france_zone4_attack_count <- france_score_counts %>%
-  filter(score_type == "attack_zone4" & !is.na(n)) %>%
-  pull(n)
-
-france_opponent_errors <- france_score_counts %>%
+test_france_opponent_errors <- test_france_score_counts %>%
   filter(grepl("error", score_type, ignore.case = TRUE) & !is.na(n)) %>%
   summarise(total = sum(n)) %>%
   pull(total)
 
-opponent_serve_errors <- france_score_counts %>%
+
+# Count Total Points Scored by France (All Games)
+total_france_score <- combined_data %>%
+  filter(scoring_team == "France") %>%
+  count(score_type, sort = TRUE)
+# Output for this is first column = score_type and second column = frequency
+# To access each individual score_type,
+# use filter(score_type == "score_type" & !is.na(n)) %>%)
+
+# Place each type of score_type into a variable
+france_score_serve_error <- total_france_score %>%
   filter(score_type == "serve_error" & !is.na(n)) %>%
   pull(n)
+# Variable contains the right amount of serve_error
 
-total_opponent_score <- opponent_score_counts %>%
+france_score_serve_ace <- total_france_score %>%
+  filter(score_type == "serve_ace" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of serve_ace
+
+france_score_attack_zone1 <- total_france_score %>%
+  filter(score_type == "attack_zone1" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of attack_zone1
+
+france_score_attack_zone2 <- total_france_score %>%
+  filter(score_type == "attack_zone2" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of attack_zone2
+
+france_score_attack_zone3 <- total_france_score %>%
+  filter(score_type == "attack_zone3" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of attack_zone3
+
+france_score_attack_zone4 <- total_france_score %>%
   filter(score_type == "attack_zone4" & !is.na(n)) %>%
   pull(n)
+# Variable contains the right amount of attack_zone4
 
-france_total_errors <- opponent_score_counts %>%
-  filter(grepl("error", score_type, ignore.case = TRUE) & !is.na(n)) %>%
-  summarise(total = sum(n)) %>%
-  pull(total)
+france_score_attack_zone5 <- total_france_score %>%
+  filter(score_type == "attack_zone5" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of attack_zone5
 
-print(paste0("Service Ace: ", france_serve_ace_count))
-print(paste0("Zone 2: ", france_zone2_attack_count))
-print(paste0("Zone 3: ", france_zone3_attack_count))
-print(paste0("Zone 4: ", france_zone4_attack_count))
-print(paste0("Opponent Error Total: ", france_opponent_errors))
-print(paste0("Opponent Service Error: ", opponent_serve_errors))
-print(paste0("Opp. Zone 4: ", total_opponent_score))
-print(paste0("France Total Errors: ", france_total_errors))
+france_score_attack_zone6 <- total_france_score %>%
+  filter(score_type == "attack_zone6" & !is.na(n)) %>%
+  pull(n)
+# Variable contains the right amount of attack_zone6
