e5698f477bce67202d7c795da9fa4a558d846d9e
wobinwob <152148300+wobinwob@users.noreply.github.com>
2025-05-03 10:51:14 +0800

    added new functions to test.R

--- FinalProject/test.R
+++ FinalProject/test.R
@@ -12,6 +12,10 @@ france_score_counts <- combined_data %>%
   filter(scoring_team == "France") %>%
   count(score_type, sort = TRUE)
 
+opponent_score_counts <- combined_data %>%
+  filter(scoring_team != "France") %>%
+  count(score_type, sort = TRUE)
+
 france_serve_ace_count <- france_score_counts %>%
   filter(score_type == "serve_ace" & !is.na(n)) %>%
   pull(n)
@@ -20,5 +24,37 @@ france_zone2_attack_count <- france_score_counts %>%
   filter(score_type == "attack_zone2" & !is.na(n)) %>%
   pull(n)
 
-print(france_serve_ace_count)
-print(france_zone2_attack_count)
+france_zone3_attack_count <- france_score_counts %>%
+  filter(score_type == "attack_zone3" & !is.na(n)) %>%
+  pull(n)
+
+france_zone4_attack_count <- france_score_counts %>%
+  filter(score_type == "attack_zone4" & !is.na(n)) %>%
+  pull(n)
+
+france_opponent_errors <- france_score_counts %>%
+  filter(grepl("error", score_type, ignore.case = TRUE) & !is.na(n)) %>%
+  summarise(total = sum(n)) %>%
+  pull(total)
+
+opponent_serve_errors <- france_score_counts %>%
+  filter(score_type == "serve_error" & !is.na(n)) %>%
+  pull(n)
+
+total_opponent_score <- opponent_score_counts %>%
+  filter(score_type == "attack_zone4" & !is.na(n)) %>%
+  pull(n)
+
+france_total_errors <- opponent_score_counts %>%
+  filter(grepl("error", score_type, ignore.case = TRUE) & !is.na(n)) %>%
+  summarise(total = sum(n)) %>%
+  pull(total)
+
+print(paste0("Service Ace: ", france_serve_ace_count))
+print(paste0("Zone 2: ", france_zone2_attack_count))
+print(paste0("Zone 3: ", france_zone3_attack_count))
+print(paste0("Zone 4: ", france_zone4_attack_count))
+print(paste0("Opponent Error Total: ", france_opponent_errors))
+print(paste0("Opponent Service Error: ", opponent_serve_errors))
+print(paste0("Opp. Zone 4: ", total_opponent_score))
+print(paste0("France Total Errors: ", france_total_errors))
